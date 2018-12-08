dc55f2fc27f9ec5111c4e877c21c16f6aab0cf97
yangliwei123 <930818274@qq.com>
2018-12-08 20:01:00 +0800

    test
    test11

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+		//1.开发项目，名称
+		define('APP_NAME','/lct');
+		//2.确定应用路径
+		define('APP_PATH','./Show/');
+		//3.开启调试模式
+		define('APP_DEBUG',true);
+		//4.应用核心文件
+		require_once './ThinkPHP/ThinkPHP.php';
+?>
